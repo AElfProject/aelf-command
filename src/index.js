3401ab027a8dfaf4aa44398fbd207a199158aabf
atom-young <atom.yang@hoopox.com>
2019-07-30 18:37:07 +0800

    feat: add publickey and log

--- src/index.js
+++ src/index.js
@@ -9,10 +9,27 @@ const generateKeyStore = (password, dir) => {
   const store = keyStore.getKeystore(wallet, password);
   let dirname = dir ? path.resolve(dir) : process.env.AELF_CLI_DATADIR;
   dirname = dirname ? path.resolve(dirname) : './';
-  const filePath = path.resolve(dirname, `${wallet.address}_keyStore.json`);
+  const filePath = path.resolve(dirname, `${wallet.address}.json`);
   fs.writeFileSync(filePath, JSON.stringify(store, null, 4));
+  wallet.publicKey = wallet.keyPair.getPublic();
+  console.log('Your wallet info is :');
+  console.log(`Mnemonic            : ${wallet.mnemonic}`);
+  console.log(`Private Key         : ${wallet.privateKey}`);
+  console.log(`Public Key          : ${wallet.publicKey}`);
+  console.log(`Address             : ${wallet.address}`);
   return {
     wallet,
     keyStore: store
   };
+};
+
+const extractFromKeyStore = (filePath, password) => {
+  const file = filePath ? path.resolve(filePath) : process.env.AELF_CLI_DATADIR;
+  const store = JSON.parse(fs.readFileSync(file).toString());
+  return keyStore.unlockKeystore(store, password);
+};
+
+module.exports = {
+  generateKeyStore,
+  extractFromKeyStore
 };
