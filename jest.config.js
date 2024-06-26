7d8c4f23a56735cc19652be4f9d175346f4a1baf
AbigailDeng <Abigail.deng@ienyan.com>
2024-06-26 12:36:34 +0800

    feat: coveragePathIgnorePatterns

--- jest.config.js
+++ jest.config.js
@@ -30,9 +30,7 @@ export default {
   coverageDirectory: 'coverage',
 
   // An array of regexp pattern strings used to skip coverage collection
-  // coveragePathIgnorePatterns: [
-  //   "/node_modules/"
-  // ],
+  coveragePathIgnorePatterns: ['/node_modules/', '/src/utils/constants.js', '/src/command/index.js'],
 
   // A list of reporter names that Jest uses when writing coverage reports
   // coverageReporters: [
